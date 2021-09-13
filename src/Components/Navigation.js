19b2c5852c95ce41f3c3ca132061e8c562cc0f02
Jayakrishnan M <Jayakrishnan1012@gmail.com>
2021-09-13 14:24:20 +0530

    Sidebar added

---
+++ src/Components/Navigation.js
@@ -0,0 +1,123 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import avatar from "../img/avatar-3.jpg";
+
+function Navigation() {
+  return (
+    <NavigationStyled>
+      <div className="avatar">
+        <img src={avatar} alt="" />
+      </div>
+      <ul className="nav-items">
+        <li className="nav-item">
+          <NavLink to="/home" activeClassName="active-class">
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/about" activeClassName="active-class">
+            About
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/projects" activeClassName="active-class">
+            Projects
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/resume" activeClassName="active-class">
+            Resume
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/blogs" activeClassName="active-class">
+            Blogs
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/contact" activeClassName="active-class">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+
+      <footer className="footer">
+        <p>&#169; 2021 Portfolio -v1</p>
+      </footer>
+    </NavigationStyled>
+  );
+}
+
+const NavigationStyled = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  border-right: 1px solid var(--border-color);
+
+  .avatar {
+    width: 100%;
+    border-bottom: 1px solid var(--border-color);
+    text-align: center;
+    padding: 1rem 0;
+    img {
+      /* height: 200px; */
+      width: 60%;
+      border-radius: 50%;
+      border: 8px solid var(--border-color);
+    }
+  }
+
+  .nav-items {
+    width: 100%;
+    text-align: center;
+    .active-class {
+      background-color: var(--primary-color);
+    }
+    li {
+      display: block;
+      a {
+        display: block;
+        padding: 0.2rem 0;
+        position: relative;
+        z-index: 4;
+
+        &:hover {
+          cursor: pointer;
+        }
+        &::before {
+          content: "";
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 0;
+          height: 50%;
+          background-color: var(--primary-color);
+          transition: all 0.4s cubic-bezier(0.94, 0.01, 0.45, 0.9);
+          z-index: 3;
+          opacity: 0.21;
+        }
+      }
+      a:hover::before {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+  footer {
+    text-align: center;
+    border-top: 1px solid var(--border-color);
+    width: 100%;
+    p {
+      padding: 1rem 0;
+      font-size: 0.75rem;
+      display: block;
+    }
+  }
+`;
+
+export default Navigation;
