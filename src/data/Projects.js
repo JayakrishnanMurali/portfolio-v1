2403a92bd07fefab2cd6620d67455b37d59f2089
Jayakrishnan M <Jayakrishnan1012@gmail.com>
2021-09-18 19:07:06 +0530

    added new project

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,11 +1,21 @@
 import insta from "../img/instagram-clone.PNG";
 import netflix from "../img/netflix-clone.PNG";
 import amazon from "../img/amazon-clone.PNG";
+import note from "../img/noteCloud.PNG";
 
 const projects = [
   {
     id: 1,
-    category: "React",
+    category: "Tailwind",
+    image: note,
+    gitLink: "https://github.com/JayakrishnanMurali/note.cloud",
+    appLink: "https://trusting-hodgkin-a133c0.netlify.app/",
+    title: "Note Cloud",
+    text: "Music Streaming App",
+  },
+  {
+    id: 2,
+    category: "CSS",
     image: insta,
     gitLink: "https://github.com/JayakrishnanMurali/instagram-clone",
     appLink: "https://gifted-stonebraker-26a7d9.netlify.app/",
@@ -13,8 +23,8 @@ const projects = [
     text: "Built with React and Firebase",
   },
   {
-    id: 2,
-    category: "React",
+    id: 3,
+    category: "CSS",
     image: netflix,
     gitLink: "https://github.com/JayakrishnanMurali/netflix-clone",
     appLink: "https://hungry-elion-8318b3.netlify.app/",
@@ -22,8 +32,8 @@ const projects = [
     text: "Built with React and Firebase",
   },
   {
-    id: 3,
-    category: "React",
+    id: 4,
+    category: "CSS",
     image: amazon,
     gitLink: "https://github.com/JayakrishnanMurali/amazon-clone",
     appLink: "https://nostalgic-tesla-5c4b86.netlify.app/",
